cc45b0b2346c86c083ab909599ae2623c0a7cff4
= <=>
2026-06-23 23:29:32 +0200

    Physics starts to be fun. Too bad I would no finish

--- scripts/vehicle/vehicle.gd
+++ scripts/vehicle/vehicle.gd
@@ -1,11 +1,154 @@
 class_name Vehicle
 extends RigidBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var cornering_force: float = 15.0
+@export var max_torque: float = 120.0
+@export var acceleration_force: float = 30.0
+@export var friction_force: float = 6.0
+@export var side_speed_drift: float = 6.0
+@export var side_speed_grip: float = 3.0
+@export var linear_damping: float = 1.0
+@export var suspension_damping: float = 6.0
+@export var tilt_ratio: float = 0.3
+@export var wheels: Array[Wheel] = []
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var speed : float:
+	get: return abs(signed_speed)
+	set(_value): assert(false, "speed is read-only")
+var signed_speed: float:
+	get: return linear_velocity.dot(-transform.basis.z)
+	set(_value): assert(false, "signed_speed is read-only")
+var side_speed: float:
+	get: return linear_velocity.dot(_get_side_vector())
+	set(_value): assert(false, "side_speed is read-only")
+var current_acceleration: float:
+	get: return _current_acceleration
+	set(_value): assert(false, "current_acceleration is read-only")
+
+var _front_raycast: RayCast3D
+var _back_raycast: RayCast3D
+var _left_raycast: RayCast3D
+var _right_raycast: RayCast3D
+
+var _floor_normal: Vector3
+var _current_torque: float
+var _acceleration_sign: float
+var _current_friction: float
+var _is_drifting: bool
+var _current_acceleration: float
+var _previous_speed: float
+
+func _ready() -> void:
+	_front_raycast = %FrontRayCast
+	_back_raycast = %BackRayCast
+	_left_raycast = %LeftRayCast
+	_right_raycast = %RightRayCast
+
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		apply_force(Vector3.UP * 5000.0)
+
+	_handle_inputs(delta)
+	_calculate_floor_normal()
+	_handle_acceleration()
+	_handle_cornering()
+	_add_side_friction_force(delta)
+	_handle_drift()
+	_apply_tilt_tweak()
+
+func _physics_process(delta: float) -> void:
+	var forward_speed: float = speed
+	_current_acceleration = (forward_speed - _previous_speed) / delta
+	_previous_speed = forward_speed
+	print("acceleration: ", _current_acceleration)
+
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if !_on_ground(): return
+	
+	var v: Vector3 = state.linear_velocity
+	var up_axis: Vector3 = global_transform.basis.y.normalized()
+	var up_velocity: Vector3 = up_axis * v.dot(up_axis)
+	state.linear_velocity -= up_velocity * suspension_damping * state.step
+
+func _handle_inputs(delta: float) -> void:
+	_acceleration_sign = (
+		1.0 if Input.is_action_pressed("ui_up")
+		else -1.0 if Input.is_action_pressed("ui_down")
+		else 0.0
+	)
+	_current_torque = lerpf(_current_torque, max_torque * _acceleration_sign, acceleration_force * delta)
+
+func _handle_acceleration() -> void:
+	# if _on_ground(): 
+	# 	if speed > 0.5 || _acceleration_sign != 0.0: linear_damp = linear_damping
+	# 	elif speed < 0.5 && _acceleration_sign == 0.0: linear_damp = 10.0 if speed > 0.25 else 30.0
+	# else: linear_damp = 0.0
+	linear_damp = linear_damping if _on_ground() else 0.0
+
+	var forward_vector: Vector3 = _get_forward_vector().normalized()
+	apply_central_force(forward_vector * _current_torque)
+
+func _handle_cornering() -> void:
+	var input_direction: Vector2 = Input.get_vector("ui_right", "ui_left", "ui_up", "ui_down", 0.2)
+	var force: float = cornering_force
+	if !_on_ground(): force /= 4.0
+	elif speed < 8.0: force *= speed * 0.125
+	apply_torque(Vector3(0.0, input_direction.x, 0.0) * deg_to_rad(90.0) * force)
+
+func _add_side_friction_force(delta: float) -> void:
+	if !_on_ground: return
+	var friction_target: float = 0.0 if _is_drifting else friction_force
+
+	# Increase friction at lower speed to limit slow drift
+	if speed < 8.0:
+		friction_target = (
+			friction_target + (friction_target * (8.0 - speed))
+			if !_is_drifting
+			else friction_force * (8.0 - speed) / 10.0
+		)
+
+	_current_friction = lerpf(_current_friction, friction_target, delta * 10.0)
+	apply_central_force(_get_side_vector() * -side_speed * mass * _current_friction)
+
+func _handle_drift() -> void:
+	if !_is_drifting && (abs(side_speed) > side_speed_drift || !_on_ground()):
+		_is_drifting = true
+	elif _is_drifting && abs(side_speed) < side_speed_grip:
+		_is_drifting = false
+
+func _apply_tilt_tweak() -> void:
+	var input_direction: Vector2 = Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up", 0.2)
+	var tilt: float = tilt_ratio * speed * 0.125 if speed < 8.0 else tilt_ratio
+	var drift_direction: float = sign(side_speed)
+	var tilt_x: float = drift_direction if _is_drifting else -input_direction.x
+
+	center_of_mass = Vector3(tilt_x * tilt / 2.0, center_of_mass.y, -_acceleration_sign * tilt)
+
+func _get_forward_vector() -> Vector3:
+	if !_front_raycast.is_colliding() && !_back_raycast.is_colliding():
+		return Vector3.ZERO
+
+	return _back_raycast.get_collision_point().direction_to(
+		_front_raycast.get_collision_point()
+	)
+
+func _get_side_vector() -> Vector3:
+	if !_left_raycast.is_colliding() && !_right_raycast.is_colliding():
+		return Vector3.ZERO
+
+	return _right_raycast.get_collision_point().direction_to(
+		_left_raycast.get_collision_point()
+	)
+
+func _on_ground() -> bool:
+	var wheels_on_ground_count: int = 0
+	for w: Wheel in wheels: 
+		if w.on_ground(): wheels_on_ground_count += 1
+	return wheels_on_ground_count > 2
+
+func _calculate_floor_normal() -> void:
+	_floor_normal = Vector3.UP
+	if _front_raycast.is_colliding():
+		_floor_normal = _front_raycast.get_collision_normal()
 
--- scripts/vehicle/wheel.gd
+++ scripts/vehicle/wheel.gd
@@ -2,36 +2,40 @@ class_name Wheel
 extends Node3D
 
 @export var parent_vehicle: Vehicle
-@export var suspension_length: float = 0.6
-@export var suspension_force: float = 180.0
+@export var suspension_length: float = 0.7
+@export var suspension_force: float = 150.0
+@export var wheel_min_offset_y: float = -0.2
 
 var _raycast: RayCast3D
+var _wheel_mesh: MeshInstance3D
+var _wheel_radius: float
+var _wheel_distance_to_raycast: float
 
 func _ready() -> void:
 	_raycast = %RayCast3D
+	_wheel_mesh = %Wheel_FL
+	_wheel_radius = _wheel_mesh.get_aabb().size.y / 2.0
+	_wheel_distance_to_raycast = _raycast.global_position.distance_to(_wheel_mesh.global_position)
 
 func _process(delta: float) -> void:
 	if parent_vehicle == null: return
+	
+	if on_ground(): _apply_suspension_force(delta)
+	_update_wheel_offset(delta)
+	
+func on_ground() -> bool:
+	if !_raycast.is_colliding(): return false
+	elif _get_suspension_distance_to_collision() > suspension_length: return false
+	return true
 
-	_handle_suspension(delta)
-
-func _handle_suspension(delta: float) -> void:
-	# update_wheel_global_position(wheel_component, delta)
-
-	var current_distance_to_collision: float = _get_suspension_distance_to_collision()
-	if !_raycast.is_colliding(): return
-
-	if current_distance_to_collision > suspension_length: return
+func _apply_suspension_force(delta: float) -> void:
+	var collision_distance: float = _get_suspension_distance_to_collision()
 
 	var collision_point: Vector3 = _raycast.get_collision_point()
-	var force_ratio: float = 1.0 - (
-		current_distance_to_collision /
-		suspension_length
-	)
+	var force_ratio: float = 1.0 - (collision_distance / suspension_length)
 
 	var direction: Vector3 = collision_point.direction_to(
 		_raycast.global_position).normalized()
-
 
 	parent_vehicle.apply_force(
 		direction * force_ratio * suspension_force,
@@ -41,3 +45,12 @@ func _handle_suspension(delta: float) -> void:
 func _get_suspension_distance_to_collision() -> float:
 	if !_raycast.is_colliding(): return suspension_length;
 	return _raycast.global_position.distance_to(_raycast.get_collision_point())
+	
+func _update_wheel_offset(delta: float) -> void:
+	if !on_ground():
+		_wheel_mesh.position.y = lerpf(_wheel_mesh.position.y, wheel_min_offset_y, delta * 20.0)
+	else:
+		var distance: float = _raycast.get_collision_point().distance_to(_raycast.global_position)
+		distance -= _wheel_distance_to_raycast
+		_wheel_mesh.position.y = lerpf(_wheel_mesh.position.y, -distance + _wheel_radius / 2.0, delta * 20.0)
+		# _wheel_mesh.global_position.y = lerpf(_wheel_mesh.global_position.y, target_y, delta * 30.0)
